9de42a5a1347f2a37a9e7f584e20090f5241f000
lorenzomezzini <60975296+lorenzomezzini@users.noreply.github.com>
2021-10-17 11:25:24 +0200

    Update train_phase1_FCNet.py

--- train_phase1_FCNet.py
+++ train_phase1_FCNet.py
@@ -11,10 +11,8 @@ from rl.models.tf.fcnet import FCNet
 
 
 def get_conf():
-    lr = 1e-4
-
     return {
-        **get_base_ppo_conf(num_workers=3),
+        **get_base_ppo_conf(num_workers=2),
         'env_config': ENV_PHASE_ONE,
     }
 
